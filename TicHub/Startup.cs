c96a5f71cfe5df90320afa029e1a7fba178fd043
ChePang Lin <jp810412@hotmail.com>
2020-08-18 14:51:23 +0800

    initial

---
+++ TicHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicHub.Startup))]
+namespace TicHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
